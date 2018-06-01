9a227954fe3da58c5ecf00ee1fac8b3e07ad8bcb
Alberto Miedes <albertomg994@gmail.com>
2018-06-01 15:51:15 +0200

    Update setup script

--- script/setup.sh
+++ script/setup.sh
@@ -1,1 +1,21 @@
-# creates symlinks for Gobierto
+#!/bin/bash
+
+echo "Running setup script for gobierto-gencat-engine"
+
+GOBIERTO_ENGINES_PATH=$DEV_DIR/gobierto/vendor/gobierto_engines
+
+if [ -z "$DEV_DIR" ]
+then
+  echo "Please set DEV_DIR in your .bash_profile before running this script";
+else
+  echo "Using DEV_DIR: $DEV_DIR"
+  echo "Using GOBIERTO_ENGINES_PATH: $GOBIERTO_ENGINES_PATH"
+
+  echo "Creating necessary directories..."
+  mkdir -p $GOBIERTO_ENGINES_PATH/gobierto-gencat-engine
+
+  echo "Creating symlinks..."
+  ln -s $DEV_DIR/gobierto-gencat-engine/app $GOBIERTO_ENGINES_PATH/gobierto-gencat-engine/app
+
+  echo "[OK]"
+fi
