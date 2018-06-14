12fd59de00db9645c9c9dcb1afd356f0c2d75750
Alberto Miedes <albertomg994@gmail.com>
2018-06-14 15:10:01 +0200

    Integrate more visualizations

--- app/javascripts/gencat/modules/gp_gencat_departments_controller.js
+++ app/javascripts/gencat/modules/gp_gencat_departments_controller.js
@@ -5,33 +5,40 @@ window.GobiertoPeople.GencatDepartmentsController = (function() {
 
   function GencatDepartmentsController() {}
 
-  GencatDepartmentsController.prototype.index = function() {
-    _loadPunchcard();
-  };
-
-  function _loadPunchcard() {
+  GencatDepartmentsController.prototype.index = function(options) {
     $(document).on('turbolinks:load', function() {
-
-      $.getJSON("gobierto_people/api/v1/people", (data) => {
-        data.sort((a, b) => a.value - b.value)
-        rowchart("#people_events_rowchart", data);
-      });
-
-      $.getJSON("gobierto_people/api/v1/people?include_history=true&limit=14", (data) => {
-        var intervalLength = 3
-        let opts = {
-          title: 'Reunions per alt càrrec i mes',
-          xTickFormat: (d, i, arr) => {
-            let distanceFromEnd = arr.length - i - 1
-            return ((distanceFromEnd % intervalLength) === 0) ? d3.timeFormat("%b %y")(d) : null
-          }
-        }
-        punchcard("#department_people_events_punchcard", data, opts)
-      });
-
+      _loadRowchart('#people_events_rowchart', options.people_events_api_url)
     });
   }
 
+  GencatDepartmentsController.prototype.show = function(options) {
+    $(document).on('turbolinks:load', function() {
+      _loadRowchart('#department_people_events_rowchart', options.department_people_events_rowchart_api_url)
+      _loadRowchart('#department_interest_groups_events_rowchart', options.department_interest_groups_rowchart_api_url)
+      _loadPunchcard('#department_people_events_punchcard', options.department_people_events_punchcard_api_url)
+    });
+  };
+
+  function _loadRowchart(container, url) {
+    $.getJSON(url, (data) => {
+      data.sort((a, b) => a.value - b.value)
+      rowchart(container, data);
+    });
+  }
+
+  function _loadPunchcard(container, url) {
+    $.getJSON(url, (data) => {
+      var intervalLength = 3
+      let opts = {
+        title: 'Reunions per alt càrrec i mes',
+        xTickFormat: (d, i, arr) => {
+          let distanceFromEnd = arr.length - i - 1
+          return ((distanceFromEnd % intervalLength) === 0) ? d3.timeFormat("%b %y")(d) : null
+        }
+      }
+      punchcard(container, data, opts)
+    });
+  }
   return GencatDepartmentsController;
 })();
 
--- app/javascripts/gencat/modules/gp_gencat_welcome_controller.js
+++ app/javascripts/gencat/modules/gp_gencat_welcome_controller.js
@@ -1,26 +1,36 @@
 import { rowchart, punchcard } from 'visualizations'
+import { d3 } from 'shared'
 
 window.GobiertoPeople.GencatWelcomeController = (function() {
 
   function GencatWelcomeController() {}
 
-  GencatWelcomeController.prototype.index = function() {
-    _loadRowcharts();
+  GencatWelcomeController.prototype.index = function(options) {
+    $(document).on('turbolinks:load', function() {
+      _loadRowchart('#interest_groups_events_rowchart', options.interest_groups_events_rowchart_api_url)
+      _loadRowchart('#people_events_rowchart', options.people_events_rowchart_api_url)
+      _loadPunchcard('#people_events_punchcard', options.department_people_events_punchcard_api_url);
+    });
   };
 
-  function _loadRowcharts() {
-    $(document).on('turbolinks:load', function() {
+  function _loadRowchart(container, url) {
+    $.getJSON(url, (data) => {
+      data.sort((a, b) => a.value - b.value)
+      rowchart(container, data);
+    });
+  }
 
-      $.getJSON("gobierto_people/api/v1/interest_groups", (data) => {
-        data.sort((a, b) => a.value - b.value)
-        rowchart("#interest_groups_events_rowchart", data);
-      });
-
-      $.getJSON("gobierto_people/api/v1/people", (data) => {
-        data.sort((a, b) => a.value - b.value)
-        rowchart("#people_events_rowchart", data);
-      });
-
+  function _loadPunchcard(container, url) {
+    $.getJSON(url, (data) => {
+      var intervalLength = 3
+      let opts = {
+        title: 'Reunions per alt càrrec i mes',
+        xTickFormat: (d, i, arr) => {
+          let distanceFromEnd = arr.length - i - 1
+          return ((distanceFromEnd % intervalLength) === 0) ? d3.timeFormat("%b %y")(d) : null
+        }
+      }
+      punchcard(container, data, opts)
     });
   }
 
