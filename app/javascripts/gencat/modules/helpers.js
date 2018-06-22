0453c5b365b6ec091535bf4477fe7d3f8caa6d47
Hugoren Martinako <aumpfbahn@gmail.com>
2018-06-22 18:27:59 +0200

    filter dates

--- app/javascripts/gencat/modules/helpers.js
+++ app/javascripts/gencat/modules/helpers.js
@@ -1,5 +1,5 @@
 import { rowchart, punchcard } from 'visualizations'
-import { d3 } from 'shared'
+import { d3, moment } from 'shared'
 
 function _loadRowchart(container, url) {
   $.getJSON(url, (data) => {
@@ -18,6 +18,13 @@ function _loadPunchcard(container, url, title) {
         return ((distanceFromEnd % intervalLength) === 0) ? d3.timeFormat("%b %y")(d) : null
       }
     }
+
+    // filter data up to 18m back
+    let mostRecentDate = _.max(_.map(_.flatten(_.concat(_.map(data, 'value'))), 'key'))
+    for (var i = 0; i < data.length; i++) {
+      data[i].value = data[i].value.filter((i) => moment(i.key).isAfter(moment(mostRecentDate).subtract(18, 'months')))
+    }
+
     punchcard(container, data, opts)
   });
 }
