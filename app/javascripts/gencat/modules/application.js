9772278522d91eff6a15735d2a2e0309737837d8
Alberto Miedes <albertomg994@gmail.com>
2018-07-20 11:03:00 +0200

    More fixes

--- app/javascripts/gencat/modules/application.js
+++ app/javascripts/gencat/modules/application.js
@@ -13,6 +13,14 @@ $(document).on('turbolinks:load', function() {
   if (currentLocationMatches('welcome_index')) {
     window.GobiertoPeople.gencat_map_controller.index(mapControllerIndexParams);
     window.GobiertoPeople.gencat_welcome_controller.index(welcomeControllerIndexParams);
+  } else if (currentLocationMatches('interest_groups_index')) {
+    window.GobiertoPeople.gencat_interest_groups_controller.index(interestGroupsIndexArgs);
+  } else if (currentLocationMatches('interest_groups_show')) {
+    window.GobiertoPeople.gencat_interest_groups_controller.show(interestGroupsShowArgs);
+  } else if (currentLocationMatches('departments_index')) {
+    window.GobiertoPeople.gencat_departments_controller.index(departmentsIndexArgs);
+  } else if (currentLocationMatches('departments_show')) {
+    window.GobiertoPeople.gencat_departments_controller.show(departmentsShowArgs);
   }
 
 });
