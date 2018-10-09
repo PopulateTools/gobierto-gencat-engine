f6be5cac9208bf45b0f29ae009d4033b97d9b726
Alberto Miedes <albertomg994@gmail.com>
2018-10-09 14:54:09 +0200

    Fix gencat setup script

--- script/setup.sh
+++ script/setup.sh
@@ -9,25 +9,25 @@ while getopts “:d:” opt; do
   esac
 done
 
-dev_dir=${opt_dir:-$DEV_DIR}
+gobierto_dir=${opt_dir:-"$DEV_DIR/gobierto"}
 
-if [ -z "$dev_dir" ]
+if [ -z "$gobierto_dir" ]
 then
-  echo "Please set DEV_DIR in your .bash_profile before running this script or invoke it with -d dev_dir, where dev_dir is the path containing gobierto";
+  echo "Please set DEV_DIR in your .bash_profile before running this script or invoke it with -d gobierto_dir, where gobierto_dir is the path of gobierto release";
 else
-  engines_path=${GOBIERTO_ENGINES_PATH:-"$DEV_DIR/gobierto/vendor/gobierto_engines"}
+  engines_path="$gobierto_dir/vendor/gobierto_engines"
   source_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
-  echo "Using dev_dir: $dev_dir"
+  echo "Using gobierto_dir: $gobierto_dir"
   echo "Using engines_path: $engines_path"
 
   echo "Creating symlinks..."
   echo "executing ln -s $source_path $engines_path"
   ln -s $source_path $engines_path
 
-  $source_path/script/create_webpacker_symlinks.sh -d $dev_dir/gobierto
+  #$source_path/script/create_webpacker_symlinks.sh -d $gobierto_dir
 
-  echo "executing ln -s $dev_dir/gobierto/node_modules $source_path"
-  ln -s $dev_dir/gobierto/node_modules $source_path
+  echo "executing ln -s $source_path/node_modules $gobierto_dir"
+  ln -s $source_path/node_modules $gobierto_dir
 
   echo "[OK]"
 fi
