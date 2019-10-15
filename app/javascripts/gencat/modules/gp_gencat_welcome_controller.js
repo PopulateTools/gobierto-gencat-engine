3688f8fb352f4e187e6f3399aab3459917f7df29
Hugoren Martinako <aumpfbahn@gmail.com>
2019-10-15 19:17:10 +0200

    boxes (wip)

--- app/javascripts/gencat/modules/gp_gencat_welcome_controller.js
+++ app/javascripts/gencat/modules/gp_gencat_welcome_controller.js
@@ -5,19 +5,19 @@ window.GobiertoPeople.GencatWelcomeController = (function() {
   function GencatWelcomeController() {}
 
   GencatWelcomeController.prototype.index = function(options) {
-    _loadRowchart('#departments_events_rowchart', options.departments_events_rowchart_api_path)
-    _loadRowchart('#interest_groups_events_rowchart', options.interest_groups_events_rowchart_api_path)
-    _loadRowchart('#people_events_rowchart', options.people_events_rowchart_api_path)
-    _loadPunchcard(
-      '#people_events_punchcard',
-      options.department_people_events_punchcard_api_path,
-      I18n.t('gobierto_people.welcome.index.punchcard_title')
-    );
+    // _loadRowchart('#departments_events_rowchart', options.departments_events_rowchart_api_path)
+    // _loadRowchart('#interest_groups_events_rowchart', options.interest_groups_events_rowchart_api_path)
+    // _loadRowchart('#people_events_rowchart', options.people_events_rowchart_api_path)
+    // _loadPunchcard(
+    //   '#people_events_punchcard',
+    //   options.department_people_events_punchcard_api_path,
+    //   I18n.t('gobierto_people.welcome.index.punchcard_title')
+    // );
 
-    // REVIEW: Waiting for render
-    setTimeout(function () {
-      setTooltipColor()
-    }, 1000);
+    // // REVIEW: Waiting for render
+    // setTimeout(function () {
+    //   setTooltipColor()
+    // }, 1000);
   };
 
   return GencatWelcomeController;
